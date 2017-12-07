066154fd7309d330e945f732f26d8d11a8e27b5b
CXJean <908625675@qq.com>
2017-12-07 13:36:12 +0800

    Initial commit

---
+++ app/src/main/java/com/xxx/text/residelayoutdemo/MainActivity.java
@@ -0,0 +1,99 @@
+package com.xxx.text.residelayoutdemo;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final ResideLayout resideLayout = findViewById(R.id.reside_layout);
+
+//        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+//        pager.setAdapter(new SimplePagerAdapter(getSupportFragmentManager()));
+        ListView menu = (ListView) findViewById(R.id.menu);
+        menu.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[] {
+                "主题", "关于", "我的"
+        }));
+        menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Menu " + ++position + " selected.",
+                        Toast.LENGTH_SHORT).show();
+                resideLayout.closePane();
+            }
+        });
+
+    }
+    private class SimplePagerAdapter extends FragmentPagerAdapter {
+
+        public SimplePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return SimpleFragment.newInstance(i);
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+
+    }
+
+    public static class SimpleFragment extends Fragment {
+
+        private int mIndex;
+        private TextView mView;
+
+        public static SimpleFragment newInstance(int index){
+            SimpleFragment fragment = new SimpleFragment();
+            Bundle args = new Bundle();
+            args.putInt("index", index);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mIndex = getArguments().getInt("index");
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+            mView = new TextView(container.getContext());
+            mView.setBackgroundColor(Color.WHITE);
+            return mView;
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            mView.setText("Fragment: " + mIndex);
+        }
+    }
+}
